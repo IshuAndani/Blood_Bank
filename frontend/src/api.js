31c5e3353061f9e5fa3ff61b04a22f296d5cb17b
Ishu Andani <ishuandani21@gmail.com>
2025-05-29 20:46:18 +0530

    ok

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://blood-bank-viy0.onrender.com',
   withCredentials: true,
   validateStatus : function(status){
     return status < 500;
